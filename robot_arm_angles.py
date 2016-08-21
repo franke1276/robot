354444df34b03a59c7f33b5bcb80506fb17b39d5
Christian Franke <chriss@frankeonline.net>
2016-08-21 11:53:06 +0200

    joystick with angles, clean up

--- robot_arm_angles.py
+++ robot_arm_angles.py
@@ -9,17 +9,18 @@ class Robot():
   pwm = None
   offset = np.array([335,339,139,200,325,336])
   factor = np.array([113,107,-73,-104,131,-119])
-
+  current_angles = None
   def __init__(self, freq):
     self.freq = freq
     self.pwm = Adafruit_PCA9685.PCA9685()
     self.pwm.set_pwm_freq(50)
     self.reset()
   def reset(self):
-    self.current = self.translate(np.array([0,0,0,0,0,0]))
+    self.current_angles = np.array([0,0,0,0,0,0])
+    self.current = self.translate(self.current_angles)
     self.__set_angles_raw(self.current)
   def current_pos(self):
-    return self.current
+    return self.current_angles
   def __gtOrLtOrEq(self, z):
     if z > 0:
       return 1
@@ -28,6 +29,8 @@ class Robot():
     return 0
   def translate(self, angles):
     return (angles * self.factor) + self.offset
+  def change_angles(self, angles_diff):
+    self.set_angles(self.current_pos() + angles_diff)
   def set_angles(self,angles):
     translated_angles = self.translate(angles).astype(int)
     while not np.array_equal(translated_angles, self.current):
@@ -39,6 +42,7 @@ class Robot():
       self.current += step
       self.__set_angles_raw(self.current)
       sleep(0.002)
+    self.current_angles = angles
 
   def __set_angles_raw(self, values):
     #print("set values: " + str(values))
--- robot_manual.py
+++ robot_manual.py
@@ -1,40 +1,41 @@
 from time import sleep
 import readchar
 import numpy as np
-import robot_arm as ra
+import robot_arm_angles as ra
 
 print("Start robot")
 robot = ra.Robot(50)
 exit = False
+step = 0.1
 while not exit:
   a = readchar.readchar()
   if a == 'x':
     exit = True
   else:
     if a == 'q':
-      robot.set_angles(robot.current_pos() + np.array([1,0,0,0,0,0]))
+      robot.change_angles(np.array([step,0,0,0,0,0]))
     if a == 'a':
-      robot.set_angles(robot.current_pos() + np.array([-1,0,0,0,0,0]))
+      robot.change_angles(np.array([-step,0,0,0,0,0]))
     if a == 'w':
-      robot.set_angles(robot.current_pos() + np.array([0,1,-1,0,0,0]))
+      robot.change_angles(np.array([0,step,-step,0,0,0]))
     if a == 's':
-      robot.set_angles(robot.current_pos() + np.array([0,-1,+1,0,0,0]))
+      robot.change_angles(np.array([0,-step,+step,0,0,0]))
     if a == 'e':
-      robot.set_angles(robot.current_pos() + np.array([0,0,1,0,0,0]))
+      robot.change_angles(np.array([0,0,step,0,0,0]))
     if a == 'd':
-      robot.set_angles(robot.current_pos() + np.array([0,0,-1,0,0,0]))
+      robot.change_angles(np.array([0,0,-step,0,0,0]))
     if a == 'r':
-      robot.set_angles(robot.current_pos() + np.array([0,0,0,1,0,0]))
+      robot.change_angles(np.array([0,0,0,step,0,0]))
     if a == 'f':
-      robot.set_angles(robot.current_pos() + np.array([0,0,0,-1,0,0]))
+      robot.change_angles(np.array([0,0,0,-step,0,0]))
     if a == 't':
-      robot.set_angles(robot.current_pos() + np.array([0,0,0,0,1,0]))
+      robot.change_angles(np.array([0,0,0,0,step,0]))
     if a == 'g':
-      robot.set_angles(robot.current_pos() + np.array([0,0,0,0,-1,0]))
+      robot.change_angles(np.array([0,0,0,0,-step,0]))
     if a == 'y':
-      robot.set_angles(robot.current_pos() + np.array([0,0,0,0,0,1]))
+      robot.change_angles(np.array([0,0,0,0,0,step]))
     if a == 'h':
-      robot.set_angles(robot.current_pos() + np.array([0,0,0,0,0,-1]))
+      robot.change_angles(np.array([0,0,0,0,0,-step]))
     if a == 'm':
       p = raw_input("Name:")
       print(p + ": " + str(robot.current_pos()))
