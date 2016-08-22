4f7b6b4cf37ef6778565a0bf034ef5e50760e698
Christian Franke <chriss@frankeonline.net>
2016-08-22 22:45:52 +0200

    revolve

--- resolve_ik.py
+++ resolve_ik.py
@@ -1,18 +1,13 @@
 import ikpy
 import numpy as np
 from ikpy import plot_utils
-import matplotlib.pyplot as plt
-
+import sys
 
 my_chain = ikpy.chain.Chain.from_urdf_file("file.urdf")
-
-target_vector = [ 0.15, 0.15, 0.2]
+list = [float(x) for x in sys.argv[1:4]]
+print(list)
+target_vector = list
 target_frame = np.eye(4)
 target_frame[:3, 3] = target_vector
 
-print("for " + str(target_vector) + " The angles of each joints are : ", my_chain.inverse_kinematics(target_frame))
-#ax = plot_utils.init_3d_figure()
-#my_chain.plot(my_chain.inverse_kinematics(target_frame), ax, target=target_vector)
-#plt.xlim(-0.1, 0.1)
-#plt.ylim(-0.1, 0.1)
-#plt.show()
+print(str(my_chain.inverse_kinematics(target_frame).tolist()[1:7]))
