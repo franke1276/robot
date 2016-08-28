57715b0e918f8b2fe5cedc1c58314b6fe49749da
Christian Franke <chriss@frankeonline.net>
2016-08-28 21:14:33 +0200

    with tool2

--- generate_path.py
+++ generate_path.py
@@ -4,9 +4,14 @@ import sys
 from scipy.spatial import distance
 
 def ik(chain, target_vector):
-  target_frame = np.eye(4)
+  target_frame =np.array([
+             [1  , 0 , 0 , 0],
+             [0 , 1 , 0 ,  0.15],
+             [0  , 0 , 1 , 0.1],
+             [0  , 0 , 0 , 1  ]
+             ])
   target_frame[:3, 3] = target_vector
-  return chain.inverse_kinematics(target_frame, regularization_parameter=0.1).tolist()[1:7]
+  return chain.inverse_kinematics(target_frame, regularization_parameter=0.1)
 
 
 def generate_path(start_pos, end_pos, velocity, resolution):
@@ -20,9 +25,8 @@ my_chain = ikpy.chain.Chain.from_urdf_file("file-with-tool.urdf")
 
 def translate_path(start_pos, end_pos, velocity, resolution):
   path = generate_path(np.array(start_pos),np.array(end_pos), velocity,resolution)
-  translated_path = [ik(my_chain, x) for x in path]
-  for p in translated_path:
-   print(str(p)) 
+  for p in path:
+    print(str(ik(my_chain, p).tolist()[1:7])) 
 
 res = 0.0015
-translate_path([0.2, 0.2, 0.1], [-0.2, 0.2, 0.1], 1, res)
+translate_path([0, 0.15, 0.2], [-0.2, 0.15, 0.2], 1, res)
--- resolve_ik.py
+++ resolve_ik.py
@@ -1,15 +1,14 @@
+#!/usr/bin/env python
 import ikpy
 import numpy as np
 from ikpy import plot_utils
 import sys
 
-my_chain = ikpy.chain.Chain.from_urdf_file("file-with-tool.urdf")
-#0, 0.15, 0.1
-#-0.05, 0.15, 0.1
+my_chain = ikpy.chain.Chain.from_urdf_file("file-with-tool2.urdf")
 target_frame =[
-             [0  , 0 , -1 , 0],
-             [0 , 1 , 0 ,   0.2],
-             [1  , 0 , 0 , 0.1],
+             [0  , -1 , 0 , 0],
+             [1 ,   0 , 0 ,  0.2],
+             [0  ,  0 , 1 , 0.15],
              [0  , 0 , 0 , 1  ]
              ]
 print(str(my_chain.inverse_kinematics(target_frame).tolist()[1:7]))
