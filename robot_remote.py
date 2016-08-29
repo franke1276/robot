d4918043462cdad6ffedaf002c1171993ddd9870
Christian Franke <chriss@frankeonline.net>
2016-08-29 22:40:03 +0200

    remote

--- robot_remote.py
+++ robot_remote.py
@@ -4,6 +4,12 @@ import readchar
 import numpy as np
 import tornado.httpclient
 import urllib
+import ikpy
+import numpy as np
+import sys
+import ikpy.geometry_utils
+
+my_chain = ikpy.chain.Chain.from_urdf_file("file-with-tool2.urdf")
 
 http_client = tornado.httpclient.HTTPClient()
 
@@ -20,36 +26,47 @@ def reset():
 
 print("Start robot")
 exit = False
-step = 0.0015
+step = 0.015
 while not exit:
   a = readchar.readchar()
   if a == 'x':
     exit = True
   else:
-    if a == 'q':
-      change_angles([step,0,0,0,0,0])
-    if a == 'a':
-      change_angles([-step,0,0,0,0,0])
-    if a == 'w':
-      change_angles([0,step,step,0,0,0])
-    if a == 's':
-      change_angles([0,-step,-step,0,0,0])
-    if a == 'e':
-      change_angles([0,0,step,0,0,0])
-    if a == 'd':
-      change_angles([0,0,-step,0,0,0])
-    if a == 'r':
-      change_angles([0,0,0,step,0,0])
-    if a == 'f':
-      change_angles([0,0,0,-step,0,0])
-    if a == 't':
-      change_angles([0,0,0,0,step,0])
-    if a == 'g':
-      change_angles([0,0,0,0,-step,0])
-    if a == 'y':
-      change_angles([0,0,0,0,0,step])
-    if a == 'h':
-      change_angles([0,0,0,0,0,-step])
+    if a == 'k':
+      target = input("target:")
+      rot = [
+          [0, -1, 0],
+          [1, 0, 0],
+          [0, 0, 1]]
+      target_frame = ikpy.geometry_utils.to_transformation_matrix( target, rot)
+      values = my_chain.inverse_kinematics(target_frame, regularization_parameter=0.1).tolist()[1:7]
+      print(str(values))
+      set_angles(values)
+      exit_k = False
+      while not exit_k:
+        b = readchar.readchar()
+        k_step = 0.002
+        add = [0,0,0]
+        if b == 'q':
+          add = [k_step, 0 , 0] 
+        if b == 'a':
+          add = [-k_step, 0 , 0] 
+        if b == 'w':
+          add = [0,k_step,  0] 
+        if b == 's':
+          add = [0, -k_step, 0] 
+        if b == 'e':
+          add = [0,0,k_step] 
+        if b == 'd':
+          add = [0,0, -k_step] 
+        if b == 'x':
+          exit_k=True 
+        target = np.array(target) + np.array(add)
+        target_frame = ikpy.geometry_utils.to_transformation_matrix( target, rot)
+        values = my_chain.inverse_kinematics(target_frame, regularization_parameter=0.1).tolist()[1:7]
+        print(str(target) + " -> " + str(values))
+        set_angles(values)
+       
     if a == 'p':
       p = input("New pos:")
       set_angles(p)
